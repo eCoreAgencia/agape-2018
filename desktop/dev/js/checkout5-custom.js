18614f04d22f59f8e6b5a41b5ba0136d8790ffe7
Renato Oliveira <renato.rops@gmail.com>
2018-09-04 11:51:56 -0300

    Alteração na cor do item de pagamento

---
+++ desktop/dev/js/checkout5-custom.js
@@ -0,0 +1,9 @@
+// WARNING: THE USAGE OF CUSTOM SCRIPTS IS NOT SUPPORTED. VTEX IS NOT LIABLE FOR ANY DAMAGES THIS MAY CAUSE. THIS MAY BREAK YOUR STORE AND STOP SALES. IN CASE OF ERRORS, PLEASE DELETE THE CONTENT OF THIS SCRIPT.
+
+// ALTERAR A COR DO ITEM DE PAGAMENTO
+
+$(document).ready(function () {
+    if ($(location).attr('href')){
+        $('li.prog-payment').addClass('prog-id')    
+    }
+});
